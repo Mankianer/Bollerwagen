e585b484b4b70c06d360076ef5d1aa720c49aed6
Mankianer <mankianer@gmail.com>
2023-03-20 20:07:00 +0100

    add reversPin

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,15 @@
 
 #define MOTOR_PIN 9
 #define BREAK_PIN 8
+#define REVERSE_PIN 7
 
 #define DEBUG 1
 #define IS_UNO 1
 
+
 MPU6050 mpu;
 boolean isBreaking = false;
+boolean isBackWards = false;
 
 #if IS_UNO
 // Eine Funktion, um stdout auf die serielle Schnittstelle umzuleiten
@@ -56,6 +59,7 @@ void setup() {
   
   pinMode(MOTOR_PIN, OUTPUT); // Motorsteuerung
   pinMode(BREAK_PIN, OUTPUT);
+  pinMode(REVERSE_PIN, OUTPUT);
 }
 
 void loop() {
@@ -64,7 +68,6 @@ void loop() {
   attitude_t attitude = mpu.getAttitude(UNITS_DEGREES);
 
   long motorSpeed = (long)attitude.roll > -20 ? -1 : max(min(map((long)attitude.roll, -60, -45, 0, 255), 255), 0); // Motor  speed 0-255 zwischen -60 und -10 Grad
-  analogWrite(MOTOR_PIN, motorSpeed); // Motorsteuerung an Pin 9
   if(motorSpeed >= 0) {
     isBreaking = false;
     digitalWrite(BREAK_PIN, LOW);
@@ -72,6 +75,20 @@ void loop() {
     isBreaking = true;
     digitalWrite(BREAK_PIN, HIGH);
   }
+  long motorSpeedBackwards = -2;
+  if(motorSpeed == 0) {
+    motorSpeedBackwards = (long)attitude.roll > -90 ? -1 : max(min(map((long)attitude.roll, -90, -100, 0, 128), 128), 0); // Motor  speed 0-128 zwischen -90 und -100 Grad
+    if(motorSpeedBackwards > 0) {
+      motorSpeed = motorSpeedBackwards;
+      isBackWards = true;
+      digitalWrite(REVERSE_PIN, HIGH);
+    } else {
+      isBackWards = false;
+      digitalWrite(REVERSE_PIN, LOW);
+    }
+  }
+
+  analogWrite(MOTOR_PIN, motorSpeed); // Motorsteuerung an Pin 9
 
   #if DEBUG
   Serial.print(attitude.pitch);
@@ -85,6 +102,10 @@ void loop() {
   Serial.print(motorSpeed > 0 ? "AN" : "AUS");
   Serial.print(" ");
   Serial.print(isBreaking ? "BREMSEN" : "FREI");
+  Serial.print(" ");
+  Serial.print(isBackWards ? "RÜCKWÄRTS" : "VORWÄRTS");
+  Serial.print(" ");
+  Serial.print(motorSpeedBackwards);
   Serial.println();
   // printf("x: %4d, y: %4d, z: %4d \n", gyro.x, gyro.y, gyro.z);
   #endif
